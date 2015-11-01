442c95e72b02d8ce973a90a54ab17b08ddd2d58b
Ryan Morehart <ryan@moreharts.com>
2015-11-01 15:48:37 -0700

    SHA256 actually works now...

--- hash/sha256.go
+++ hash/sha256.go
@@ -5,11 +5,13 @@ import (
 	"crypto"
 	"crypto/sha256" // SHA256 will register itself
 	"io"
+	"log"
+	"os"
 
 	"github.com/traherom/gocrypt"
 )
 
-// Size of the hash output in bytes
+// Sha256Size holds the size of the hash output in bytes
 var Sha256Size = sha256.Size
 
 // Sha256 hashes the given stream and returns the result.
@@ -19,7 +21,7 @@ func Sha256(r io.Reader) (gocrypt.Hash, error) {
 	block := make([]byte, h.BlockSize())
 	for {
 		cnt, err := r.Read(block)
-		h.Sum(block[:cnt])
+		h.Write(block[:cnt])
 
 		if err == io.EOF {
 			break
@@ -29,4 +31,16 @@ func Sha256(r io.Reader) (gocrypt.Hash, error) {
 	}
 
 	return h.Sum(nil), nil
+}
+
+// Sha256File opens the given file and returns the SHA256 sum of it
+func Sha256File(path string) (gocrypt.Hash, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	log.Println("summing", path)
+	return Sha256(f)
 }
